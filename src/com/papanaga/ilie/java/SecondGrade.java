6af93fd8c824a11962d2a393396c57dddee308a2
IliePapanaga <ilie.papanaga@gmail.com>
2023-07-04 16:29:00 +0300

    new

--- src/com/papanaga/ilie/java/SecondGrade.java
+++ src/com/papanaga/ilie/java/SecondGrade.java
@@ -1,39 +1,39 @@
 package com.papanaga.ilie.java;
-
 import java.util.Scanner;
 
 public class SecondGrade {
-    public static void main(String[] args) {
+    float TerminA;
+    float TerminB;
+    float TerminC;
+
+    public void read() {
         Scanner scanner = new Scanner(System.in);
         boolean continua = true;
-
         while (continua) {
             System.out.println("Introduceți factorii ecuației de gradul 2:");
+            System.out.print("Termin a = ");
+            TerminA = scanner.nextFloat();
+            System.out.print("Termin b = ");
+            TerminB=scanner.nextFloat();
+            System.out.print("Termin c = ");
+            TerminC=scanner.nextFloat();
+        }
+        public void execute() {
 
-            System.out.print("a = ");
-            double a = scanner.nextDouble();
-
-            System.out.print("b = ");
-            double b = scanner.nextDouble();
-
-            System.out.print("c = ");
-            double c = scanner.nextDouble();
-
-            double delta = b * b - 4 * a * c;
-            if (delta > 0) {
-                double x1 = (-b + Math.sqrt(delta)) / (2 * a);
-                double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            float raspuns;
+            raspuns = TerminB * TerminB - 4 * TerminA * TerminC;
+            if (raspuns > 0) {
+                float x1 = (-TerminB + Math.sqrt(raspuns)) / (2 * TerminA);
+                float x2 = (-TerminB - Math.sqrt(raspuns)) / (2 * TerminA);
                 System.out.println("Soluțiile ecuației sunt: x1 = " + x1 + ", x2 = " + x2);
-            } else if (delta == 0) {
-                double x = -b / (2 * a);
+            } else if (raspuns == 0) {
+                float x = -TerminB / (2 * TerminA);
                 System.out.println("Ecuația are o singură soluție: x = " + x);
             } else {
                 System.out.println("Ecuația nu are soluții reale.");
             }
-
             System.out.print("Doriți să introduceți alți factori? (da/nu): ");
             String raspuns = scanner.next();
-
             if (!raspuns.equalsIgnoreCase("da")) {
                 continua = false;
                 System.out.println("Multumesc");
